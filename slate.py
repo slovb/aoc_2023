8c92e13f5e50ce9ddf36355d6831859e0ed85ee1
Rickard Nyman <4slovb@gmail.com>
2023-12-01 05:35:23 +0100

    Starting slate for babyracing

---
+++ slate.py
@@ -0,0 +1,34 @@
+def solve(input):
+    out = 0
+    for row in input:
+        pass
+    return out
+
+
+def read(filename):
+    with open(filename, "r") as f:
+        rows = [row.rstrip() for row in f.readlines()]
+        input = []
+        for row in rows:
+            # row = row.split(",")
+            # row = map(str.strip, row)
+            # row = map(int, row)
+
+            input.append(row)
+            # input.append(int(row))
+            # input.append(list(row))
+        return input
+
+
+def main(filename):
+    return solve(read(filename))
+
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) < 2:
+        print("\n{}".format(main("input.txt")))
+    else:
+        for f in sys.argv[1:]:
+            print("\n{}".format(main(f)))
